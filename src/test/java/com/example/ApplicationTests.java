ea6fdcfa24ba5ebdc57964478339e227396e88f5
Rodrigo Melo <rodrigo.melo@skipthedishes.ca>
2019-08-05 16:24:29 -0500

    Fixed ApplicationTest class.

---
+++ src/test/java/com/example/ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
+public class ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
